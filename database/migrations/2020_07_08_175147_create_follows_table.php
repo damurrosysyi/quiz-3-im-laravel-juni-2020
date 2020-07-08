049deb797ef5889f64d80b3d65c869bc2079542f
unknown <dmujahidain@gmail.com>
2020-07-09 02:24:10 +0800

    alhamdulillah finally berhasil

---
+++ database/migrations/2020_07_08_175147_create_follows_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_can_follows', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('follows');
+            $table->timestamps();
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_can_follows');
+    }
+}
